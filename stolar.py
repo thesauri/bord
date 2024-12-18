ed3ce91ca6c4c6fcf135f1df102a241e7f3ee3a2
Walter Berggren <walter@berggren.dev>
2024-12-18 20:03:52 +0200

    Run black formatter

--- stolar.py
+++ stolar.py
@@ -1,17 +1,23 @@
 import pyxel
 import dumbster
 
+
 def is_player_overlapping_circular_object(player, position, radius):
     cx, cy = position
-    return pow(cx - player.position[0], 2) + pow(cy - player.position[1], 2) < pow(radius + player.player_radius, 2)
+    return pow(cx - player.position[0], 2) + pow(cy - player.position[1], 2) < pow(
+        radius + player.player_radius, 2
+    )
 
 
 def is_player_overlapping_rectangle(player, rectangle):
     rx, ry, rw, rh = rectangle
-    return player.position[0] > rx and \
-        player.position[1] > ry and \
-        player.position[0] < rx + rw and \
-        player.position[1] < ry + rh
+    return (
+        player.position[0] > rx
+        and player.position[1] > ry
+        and player.position[0] < rx + rw
+        and player.position[1] < ry + rh
+    )
+
 
 class Game:
     def __init__(self):
@@ -38,32 +44,34 @@ class Game:
                 dumbster.get_action,
                 [
                     self.width / 2 - self.cart_width / 4 - 2,
-                    self.height - self.cart_height / 2 - 2
-                ]
+                    self.height - self.cart_height / 2 - 2,
+                ],
             ),
             Player(
                 self,
                 dumbster.get_action,
                 [
                     self.width / 2 + self.cart_width / 4 - 2,
-                    self.height - self.cart_height / 2 - 2
-                ]
-            )
+                    self.height - self.cart_height / 2 - 2,
+                ],
+            ),
         ]
 
         # Cart (for returning stuff)
         self.cart = [
-            self.width/2 - self.cart_width/2, self.height - self.cart_height,
-            self.cart_width, self.cart_height
+            self.width / 2 - self.cart_width / 2,
+            self.height - self.cart_height,
+            self.cart_width,
+            self.cart_height,
         ]
 
         # Table properties [x, y, width, height]
         self.tables = [
-            [1/4 * self.width, 1/4 * self.height],    # Top left table
-            [3/4 * self.width, 1/4 * self.height],   # Top right table
-            [1/2 * self.width, 1/2 * self.height],    # Middle
-            [1/4 * self.width , 3/4 * self.height],    # Bottom left table
-            [3/4 * self.width, 3/4 * self.height],   # Bottom right table
+            [1 / 4 * self.width, 1 / 4 * self.height],  # Top left table
+            [3 / 4 * self.width, 1 / 4 * self.height],  # Top right table
+            [1 / 2 * self.width, 1 / 2 * self.height],  # Middle
+            [1 / 4 * self.width, 3 / 4 * self.height],  # Bottom left table
+            [3 / 4 * self.width, 3 / 4 * self.height],  # Bottom right table
         ]
 
         # Chair properties [x, y, id]
@@ -72,21 +80,41 @@ class Game:
         for table in self.tables:
             tx, ty = table
             # Add four chairs: one on each side
-            self.chairs.extend([
-                [tx, ty - self.table_radius - self.chair_radius - self.chair_offset], # Top
-                [tx + self.table_radius + self.chair_radius + self.chair_offset, ty], # Right
-                [tx, ty + self.table_radius + self.chair_radius + self.chair_offset], # Bottom
-                [tx - self.table_radius - self.chair_radius - self.chair_offset, ty] # Right
-            ])
+            self.chairs.extend(
+                [
+                    [
+                        tx,
+                        ty - self.table_radius - self.chair_radius - self.chair_offset,
+                    ],  # Top
+                    [
+                        tx + self.table_radius + self.chair_radius + self.chair_offset,
+                        ty,
+                    ],  # Right
+                    [
+                        tx,
+                        ty + self.table_radius + self.chair_radius + self.chair_offset,
+                    ],  # Bottom
+                    [
+                        tx - self.table_radius - self.chair_radius - self.chair_offset,
+                        ty,
+                    ],  # Right
+                ]
+            )
 
         pyxel.run(self.update, self.draw)
 
     def is_player_hitting_wall(self, player):
         return (
-                player.position[1] < self.wall_thickness + player.player_radius or
-                player.position[0] > self.width - self.wall_thickness - player.player_radius or
-                player.position[1] > self.height - self.wall_thickness - player.player_radius or
-                player.position[0] < self.wall_thickness + player.player_radius
+            (player.position[1] < self.wall_thickness + player.player_radius)
+            or (
+                player.position[0]
+                > self.width - self.wall_thickness - player.player_radius
+            )
+            or (
+                player.position[1]
+                > self.height - self.wall_thickness - player.player_radius
+            )
+            or (player.position[0] < self.wall_thickness + player.player_radius)
         )
 
     def update(self):
@@ -96,7 +124,7 @@ class Game:
         is_all_capacity_zero = True
         for player in self.players:
             if not player.capacity == 0:
-               is_all_capacity_zero = False
+                is_all_capacity_zero = False
 
         if is_all_capacity_zero and len(self.chairs) == 0 and len(self.tables) == 0:
             self.is_game_over = True
@@ -111,13 +139,22 @@ class Game:
                 player.position = original_position
 
             for chair in self.chairs:
-                if is_player_overlapping_circular_object(player, chair, self.chair_radius) and \
-                        player.capacity + self.chair_capacity <= player.max_capacity:
+                if (
+                    is_player_overlapping_circular_object(
+                        player, chair, self.chair_radius
+                    )
+                    and player.capacity + self.chair_capacity <= player.max_capacity
+                ):
                     player.capacity += 1
                     self.chairs.remove(chair)
 
             for table in self.tables:
-                if is_player_overlapping_circular_object(player, table, self.table_radius) and player.capacity + self.table_capacity <= player.max_capacity:
+                if (
+                    is_player_overlapping_circular_object(
+                        player, table, self.table_radius
+                    )
+                    and player.capacity + self.table_capacity <= player.max_capacity
+                ):
                     player.capacity += 3
                     self.tables.remove(table)
 
@@ -136,12 +173,14 @@ class Game:
             self.wall_thickness,
             self.width - 2 * self.wall_thickness,
             self.height - 2 * self.wall_thickness,
-            0
+            0,
         )  # Inner room
 
         # Draw cart
         pyxel.rect(self.cart[0], self.cart[1], self.cart[2], self.cart[3], 1)
-        pyxel.rect(self.cart[0] + 1, self.cart[1] + 1, self.cart[2] - 2, self.cart[3] - 1, 2)
+        pyxel.rect(
+            self.cart[0] + 1, self.cart[1] + 1, self.cart[2] - 2, self.cart[3] - 1, 2
+        )
 
         # Draw chairs
         for chair in self.chairs:
@@ -157,29 +196,34 @@ class Game:
         for player in self.players:
             pyxel.circ(player.position[0], player.position[1], player.player_radius, 8)
             pyxel.circ(
-                player.position[0],
-                player.position[1],
-                player.player_radius - 1,
-                9
+                player.position[0], player.position[1], player.player_radius - 1, 9
             )
 
             # Draw capacity indicator
             for n in range(0, player.max_capacity):
                 red_if_used_otherwise_green = 8 if player.capacity > n else 11
                 pyxel.rect(
-                    2*n + player.position[0] + player.player_radius, player.position[1] - player.player_radius - 2,
-                    1, 1, red_if_used_otherwise_green
+                    2 * n + player.position[0] + player.player_radius,
+                    player.position[1] - player.player_radius - 2,
+                    1,
+                    1,
+                    red_if_used_otherwise_green,
                 )
 
         if self.is_game_over:
-            pyxel.text(self.width/2 - 18, self.height/2 - 20, "Game over", pyxel.frame_count % 16)
+            pyxel.text(
+                self.width / 2 - 18,
+                self.height / 2 - 20,
+                "Game over",
+                pyxel.frame_count % 16,
+            )
 
             for i, player in enumerate(self.players):
                 pyxel.text(
-                    self.width/2 - 32,
-                    self.height/2 + 10*i,
+                    self.width / 2 - 32,
+                    self.height / 2 + 10 * i,
                     f"Player {i+1}: {player.score} points",
-                    pyxel.frame_count % 16
+                    pyxel.frame_count % 16,
                 )
 
 
@@ -197,13 +241,7 @@ class Player:
         self.score = 0
 
     def update(self, chairs, tables, cart):
-        action = dumbster.get_action(
-            self.position,
-            self.capacity,
-            chairs,
-            tables,
-            cart
-        )
+        action = dumbster.get_action(self.position, self.capacity, chairs, tables, cart)
 
         if action == "LEFT":
             self.position[0] -= self.player_speed
@@ -213,5 +251,6 @@ class Player:
             self.position[1] -= self.player_speed
         if action == "DOWN":
             self.position[1] += self.player_speed
+
 
 Game()
